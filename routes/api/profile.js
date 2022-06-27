12d6b7458238d2f4ccd372314a03b58521a2d6d7
Ankur Kashyap <kashyap172ankur@gmail.com>
2022-06-27 15:27:57 +0530

    added more files

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -95,4 +95,37 @@ router.post('/', [auth, [
         res.status(500).send('Server Error.');
     }
 });
+
+// @route   GET api/profile
+// @route   get all profiles
+// @access  Public
+router.get('/', async(req,res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch(err) {
+        console.log(err.message);
+        res.status(500).send('Server error.');
+    }
+});
+
+// @route   GET api/profile/user/:user_id
+// @route   get profile by userid
+// @access  Public
+router.get('/user/:user_id', async(req,res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+        if(!profile) {
+            return res.status(400).json({ msg: 'Profile not found.' });
+        }
+        res.json(profile);
+    } catch(err) {
+        console.log(err.message);
+        if(err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found.' });
+        }
+        res.status(500).send('Server error.');
+    }
+});
+
 module.exports = router;
